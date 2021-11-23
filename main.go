61667538c635512d09e4377860d3bfa833956e36
Olayinka Olasunkanmi <sj.olayinka@gmail.com>
2021-11-23 12:39:16 -0500

    Created first server

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to my awesome site")
+}
+
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Server started!!!")
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":3000", nil)
 }
