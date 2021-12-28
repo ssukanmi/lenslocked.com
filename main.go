1007cc81d9a16394a7254e526fe6ed9879f7aa18
Olayinka Olasunkanmi <sj.olayinka@gmail.com>
2021-12-28 04:16:11 -0500

    Added contact page

--- main.go
+++ main.go
@@ -10,7 +10,11 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	ip := r.RemoteAddr
 	dateTime := time.Now()
-	fmt.Fprintf(w, "<h1>Welcome to GoLang</h1>Your IP: %s<br>Date: %s", ip, dateTime.Format(time.ANSIC))
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "<h1>Welcome to Homepage</h1>Your IP: %s<br>Date: %s<br>", ip, dateTime.Format(time.ANSIC))
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	}
 }
 
 func main() {
