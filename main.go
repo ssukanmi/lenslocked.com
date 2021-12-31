9d16cae28d287c70edc8cb28ca962ae79ce39764
Olayinka Olasunkanmi <sj.olayinka@gmail.com>
2021-12-31 03:34:57 -0500

    Switched to  gorilla mux

--- main.go
+++ main.go
@@ -6,37 +6,20 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	ip := r.RemoteAddr
 	dateTime := time.Now()
-	if r.URL.Path == "/" {
-		fmt.Fprintf(w, "<h1>Welcome to Homepage</h1>Your IP: %s<br>Date: %s<br>", ip, dateTime.Format(time.ANSIC))
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "<h1>We could not find the page you were looking for :(</h1>")
-	}
-}
-
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
-}
-
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+	fmt.Fprintf(w, "<h1>Welcome to Homepage</h1>Your IP: %s<br>Date: %s<br>", ip, dateTime.Format(time.ANSIC))
 }
 
 func main() {
 	fmt.Println("Server started!!!")
-	router := httprouter.New()
-	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
 
-	http.HandleFunc("/", indexHandler)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandler)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
